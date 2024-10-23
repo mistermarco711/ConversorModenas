3b8e24ae361c2bc58e519d2fec49ced414d69aea
mistermarco711 <masandoval711@gmail.com>
2024-10-23 09:17:50 -0500

    iniciando solicitudes

--- ConversorMonedas/src/Main.java
+++ ConversorMonedas/src/Main.java
@@ -1,5 +1,9 @@
+import java.net.http.HttpClient;
+
 public class Main {
     public static void main(String[] args) {
+        HttpClient client = HttpClient.newHttpClient();
+
         System.out.println("Hello world!");
     }
 }
